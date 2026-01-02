8b8036f55a12ec0254a0794363ea5f713d8301ee
David Xia <d.xia111@gmail.com>
2026-01-01 19:29:26 -0700

    Finished chapter 6

--- vec3.h
+++ vec3.h
@@ -15,7 +15,7 @@ public:
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
-    vec3 operator -() const { return vec3(-e[0], -e[1], -e[2]); }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
 
@@ -80,7 +80,7 @@ inline vec3 operator/(const vec3& v, double t) {
 }
 
 inline double dot(const vec3& u, const vec3& v) {
-    return u.e[0] * v.e[0] + v.e[1] * v.e[1] + u.e[2] * v.e[2];
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) {
